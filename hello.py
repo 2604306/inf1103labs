a50ec1a6364e5761bfd4d9df758953da7898087b
gabriel <leeg42239@gmail.com>
2026-09-04 11:03:45 +0800

    second post

--- hello.py
+++ hello.py
@@ -3,13 +3,25 @@ print("Welcome here")
 print("My first post!")
 print("==========================")
 
-followers = 100
+username = input("Enter Username: ")
+age = input("Enter Age: ")
+category = input("Enter Content Category: ")
 
-followers += 50
-print("Day 1:", followers)
+print("\nInstagram Profile")
+print("====================")
+print("Username: ", username)
+print("Age: :", age)
+print("Category: ", category)
 
-followers += 20
-print("Day 2:", followers)
 
-followers += 10
-print("Day 3:", followers)
+
+#followers = 100
+
+#followers += 50
+#print("Day 1:", followers)
+
+#followers += 20
+#print("Day 2:", followers)
+
+#followers += 10
+#print("Day 3:", followers)
