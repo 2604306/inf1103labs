53dc7925604bbf0949e272ca6531dc2dfb72993d
gabriel <leeg42239@gmail.com>
2026-09-04 10:52:05 +0800

    First Post

--- hello.py
+++ hello.py
@@ -3,10 +3,13 @@ print("Welcome here")
 print("My first post!")
 print("==========================")
 
-username = "gae_rabbits"
-bio = "rabbits that are gae"
-followers = 98721378
+followers = 100
 
-print("Username:", username)
-print("Bio:", bio)
-print("Followers:", followers)
+followers += 50
+print("Day 1:", followers)
+
+followers += 20
+print("Day 2:", followers)
+
+followers += 10
+print("Day 3:", followers)
